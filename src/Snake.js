17fc0495415843b7902176664c1f17a451db42f2
JdvazquezDev <43555010+JdvazquezDev@users.noreply.github.com>
2019-02-06 20:32:26 +0100

    Class Snake
    - Replaces functions by methods.
    - Adds class Snake that replaces object head and array body.

---
+++ src/Snake.js
@@ -0,0 +1,74 @@
+import Square from './Square.js'
+
+export default class {
+    constructor() {
+        this.head = new Square(0, 0);
+        this.body = [];
+    }
+    addElement(element){
+        this.body.push(element);
+    }
+
+    getLastElement() {
+        if (this.body.length >= 1) {
+            return this.body[this.body.length - 1];
+
+        } else {
+            return this.head;
+        }
+    }
+
+    move(dx, dy) {
+        // The snake.body follows the snake
+        // 3 elem -> body.length = 3
+        // i=3; i=2, i=1;
+        // 3<-2  2<-1 1<-0 0<-head
+        for (let i = this.body.length - 1; i >= 1; --i) {
+            this.body[i].x = this.body[i - 1].x; // elem.x 1 <- elem.x 0
+            this.body[i].y = this.body[i - 1].y; // elem.y 1 <- elem.y 0
+        }
+        if (this.body.length >= 1) {
+            this.body[0].x = this.head.x;
+            this.body[0].y = this.head.y;
+        }
+        // Update coordinates of the snake's head
+        this.head.move(dx, dy);
+    }
+
+    hasCollided() {
+        for (let i = 0; i < this.body.length; i++) {
+            if (this.body[i].checkCollision(this.head))
+                return true;
+        }
+    }
+
+    hasBrokenTheLimits(x1, x2, y1, y2) {
+        const topCollision = (this.head.y < y1); // x: ?, y: 0
+        const bottomCollision = (this.head.y > y2); //x: ?, y: myCanvas.height
+        const leftCollision = (this.head.x < x1); // x: 0, y: ?
+        const rightCollision = (this.head.x > x2); //x: myCanvas.width, y: ?
+
+        if (topCollision || bottomCollision || leftCollision || rightCollision) {
+            return true;
+        }
+    }
+
+    checkFullCollision(position) {
+        // Check if coordinates of food are the same as other elem of snake's body
+        for (let i = 0; i < this.body.length; i++) {
+            if (this.body[i].checkCollision(position)) {
+                return true;
+            }
+        }
+        // Check if coordinates of food are the same as snake's head
+        if (this.head.checkCollision(position)) {
+            return true;
+        }
+    }
+
+    reset() {
+        this.head.y = 0;
+        this.head.x = 0;
+        this.body.length = 0; // By assigning length = 0 js understands that you want to empty the array
+    }
+}
--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
 import Square from './Square.js'
+import Snake from './Snake.js'
 
 console.log('Starting Game...');
 
 const myCanvas = document.getElementById('myCanvas');
 const context = myCanvas.getContext('2d');
 
-const SIZE = 20;
+const snake = new Snake();
 
-const head = new Square(0, 0);
-const body = [];
+const SIZE = 20;
 
 let food = null; // x: y:
 
@@ -33,26 +33,12 @@ function update() {
 
     //Save previous position
     let prevX, prevY;
-    if (body.length >= 1) {
-        prevX = body[body.length - 1].x;
-        prevY = body[body.length - 1].y;
-    } else {
-        prevX = head.x;
-        prevY = head.y;
-    }
+    const lastElement = snake.getLastElement();
+    prevX = lastElement.x;
+    prevY = lastElement.y;
 
-    // The body follows the snake
-    for (let i = body.length - 1; i >= 1; --i) {
-        body[i].x = body[i - 1].x; // elem.x 1 <- elem.x 0
-        body[i].y = body[i - 1].y; // elem.y 1 <- elem.y 0
-    }
-    if (body.length >= 1) {
-        body[0].x = head.x;
-        body[0].y = head.y;
-    }
-
-    // Update coordinates of the snake's head
-    head.move(dx, dy);
+    //Makes the snake advance
+    snake.move(dx, dy);
 
     if (dx !== 0) {
         lastAxis = 'X'
@@ -61,7 +47,7 @@ function update() {
     }
 
     // Detect if snake has eaten food
-    if (food && head.checkCollision(food)) {
+    if (food && snake.head.checkCollision(food)) {
         food = null;
         // Increase the size of the snake
         increaseSnakeSize(prevX, prevY);
@@ -76,62 +62,40 @@ function update() {
 function randomFoodPosition() {
     let position;
     do {
-        position = {
-            x: getRandomX(),
-            y: getRandomY()
-        };
+        position = new Square(getRandomX(), getRandomY());
     }
     while (checkFoodCollision(position));
     return position;
 }
 
 function checkFoodCollision(position) {
-    // Check if coordinates of food are the same as other elem of snake's body
-    for (let i = 0; i < body.length; i++) {
-        if (body[i].checkCollision(position)) {
-            return true;
-        }
-    }
-    // Check if coordinates of food are the same as snake's head
-    if (head.checkCollision(position)) {
+    if (snake.checkFullCollision(position))
         return true;
-    }
 
     return false;
 }
 
 function checkSnakeCollision() {
-    // Check if coordinates of head are the same as other elem of snake's body
-    for (let i = 0; i < body.length; i++) {
-        if (head.x === body[i].x && head.y === body[i].y) {
-            return true;
-        }
-    }
+    // Check if coordinates of head are the same as other elem of snake's snake.body
+    if (snake.hasCollided())
+        return true;
 
     // Detect snake collisions against context limits
-    const topCollision = (head.y < 0); // x: ?, y: 0
-    const bottomCollision = (head.y > myCanvas.height); //x: ?, y: myCanvas.height
-    const leftCollision = (head.x < 0); // x: 0, y: ?
-    const rightCollision = (head.x > myCanvas.width); //x: myCanvas.width, y: ?
-
-    if (topCollision || bottomCollision || leftCollision || rightCollision) {
+    if (snake.hasBrokenTheLimits(0, myCanvas.width - SIZE, 0, myCanvas.height - SIZE))
         return true;
-    }
 
     return false;
 }
 
 function gameOver() {
     alert('GAME OVER!');
-    head.x = 0;
-    head.y = 0;
     dx = 0;
     dy = 0;
-    body.length = 0; // By assigning length = 0 js understands that you want to empty the array
+    snake.reset();
 }
 
 function increaseSnakeSize(prevX, prevY) {
-    body.push(
+    snake.addElement(
         new Square(prevX, prevY)
     );
 }
@@ -154,10 +118,10 @@ function draw() {
     context.fillRect(0, 0, myCanvas.width, myCanvas.height); // Clear context
 
     // Head
-    drawObject(head, 'lime');
+    drawObject(snake.head, 'lime');
 
     // Body
-    body.forEach(
+    snake.body.forEach(
         elem => drawObject(elem, 'lime')
     );
 
